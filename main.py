ca89cf87bbbae59bbe8b9367c67a59ae12b05e2d
Steffen Robohm <steffen.robohm@gmail.com>
2023-12-14 09:45:00 +0100

    Added bot training file

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
 from chatterbot import ChatBot
+from chatterbot.trainers import ListTrainer
+import re
 
-chatbot = ChatBot("Chatpot")
+chatbot = ChatBot("Chatbot")
+FILE = "chat.txt"
 
+# Read training file
+def prepareFile():  
+    with open(FILE, "r") as training_file:
+        content = training_file.read()
+    return tuple(content.split("\n"))
+
+# Train the bot
+prepared_file = prepareFile()
+trainer = ListTrainer(chatbot)
+trainer.train(prepared_file)
+
+# Main loop
 exit_conditions = (":q", "quit", "exit")
 while True:
     query = input("> ")
