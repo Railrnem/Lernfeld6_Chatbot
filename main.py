f6fe0541e9cd6a7f40688ef4af9b6564c7d751ea
Steffen Robohm <steffen.robohm@gmail.com>
2023-12-14 13:43:17 +0100

    Code cleanuo

--- main.py
+++ main.py
@@ -11,21 +11,19 @@ chatbot = ChatBot("Chatbot",
                 }
                 ],
                 read_only=True)
-FILE1 = "training_1.txt"
-FILE2 = "training_2.txt"
 
 # Read training file
-def prepareFile(FILE: str):  
-    with open(FILE, "r") as training_file:
+def prepareFile(file: str):  
+    with open(file, "r") as training_file:
         content = training_file.read()
     return tuple(content.split("\n"))
 
 # Train the bot
 trainer = ListTrainer(chatbot)
-prepared_file = prepareFile(FILE1)
-trainer.train(prepared_file)
-prepared_file = prepareFile(FILE2)
-trainer.train(prepared_file)
+files = ["training_1.txt", "training_2.txt"]
+for file in files:
+    prepared_file = prepareFile(file)
+    trainer.train(prepared_file)
 
 # Main loop
 exit_conditions = (":q", "quit", "exit")
