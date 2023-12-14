c1731ff9d7660b0236259c684a8ac1ffc48dfc4f
Steffen Robohm <steffen.robohm@gmail.com>
2023-12-14 11:31:49 +0100

    Added training data

--- main.py
+++ main.py
@@ -3,17 +3,20 @@ from chatterbot.trainers import ListTrainer
 import re
 
 chatbot = ChatBot("Chatbot")
-FILE = "chat.txt"
+FILE1 = "training_1.txt"
+FILE2 = "training_2.txt"
 
 # Read training file
-def prepareFile():  
+def prepareFile(FILE: str):  
     with open(FILE, "r") as training_file:
         content = training_file.read()
     return tuple(content.split("\n"))
 
 # Train the bot
-prepared_file = prepareFile()
 trainer = ListTrainer(chatbot)
+prepared_file = prepareFile(FILE1)
+trainer.train(prepared_file)
+prepared_file = prepareFile(FILE2)
 trainer.train(prepared_file)
 
 # Main loop
