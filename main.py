985ee956cb5af796f5ca1a2cb9eec42885951d5f
Steffen Robohm <steffen.robohm@gmail.com>
2023-12-18 10:27:06 +0100

    "I want to talk to a human"

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ chatbot = ChatBot("Chatbot",
                 logic_adapters=[
                 {
                     'import_path': 'chatterbot.logic.BestMatch',
-                    'default_response': 'I am sorry, but I do not understand.',
+                    'default_response': 'I am sorry, but I do not understand. If you want to escalte to 2nd level support please type: "I want to talk to a human"',
                     'maximum_similarity_threshold': 0.90
                 }
                 ],
@@ -20,7 +20,7 @@ def prepareFile(file: str):
 
 # Train the bot
 trainer = ListTrainer(chatbot)
-files = ["training_1.txt", "training_2.txt", "training_3.txt", "training_4.txt"]
+files = ["training_1.txt", "training_2.txt", "training_3.txt", "training_4.txt", "training_5.txt"]
 for file in files:
     prepared_file = prepareFile(file)
     trainer.train(prepared_file)
